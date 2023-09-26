1b49592bcc6519284e352b7b021b2c2f913d577a
Elnaza <helzahggl@gmail.com>
2023-09-26 13:23:07 +0300

    Добавлена Задача38

---
+++ Zadacha38/Program.cs
@@ -0,0 +1,64 @@
+﻿
+// Задача 38
+
+Console.WriteLine(" Задать одномерный массив, заполнены   случайными числами . Задать через пробелл. Найти сумму чисел на нечетных позициях.");
+string? str = Console.ReadLine();
+string[] strArr = str.Split(' ');
+
+if (CheckArrayByNumbers(strArr, out List<float> lst))
+{    
+    float delta2 = lst.Max() - lst.Min();
+
+    float delta = GetMax(lst) - GetMin(lst);
+
+    Console.WriteLine($" Разница между максимальным и минимальным элементом =  {delta}   LINQ = {delta2}");
+}
+
+float GetMin(List<float> lst)
+{
+    float min = lst[0];
+    for (int i = 1; i < lst.Count; i++)
+    {
+        if (min > lst[i])
+            min = lst[i];
+    }
+    return min;
+    
+}
+
+float GetMax(List<float> lst)
+{
+    float max = lst[0];
+    for (int i = 1; i < lst.Count; i++)
+    {
+        if (max < lst[i])
+            max = lst[i];
+    }
+    return max;
+
+}
+
+// Проверка на корректность введенных вещественых чисел, и возврат массива чисел
+bool CheckArrayByNumbers(string[] arr, out List<float> lst)
+{
+    lst = new();
+    foreach (var s in arr)
+    {
+        float number = 0;
+        try
+        {
+            number = float.Parse(s);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Было введено не число, а набор символов {s} ");
+            Console.WriteLine($"Возникло исключение {ex.Message} ");
+
+            return false;
+        }                       
+        
+        lst.Add(number);
+    }
+    return true;
+}
+
